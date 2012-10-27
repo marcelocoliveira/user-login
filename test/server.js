c643bf4413eab231d5b949bce9f802115dc2f60b
Rory Madden <rorymadden@gmail.com>
2012-10-27 12:00:44 -0300

    Added error handling and routing tests

--- test/server.js
+++ test/server.js
@@ -4,41 +4,46 @@ var app = require('../app')
   , RedisStore = require("connect-redis")(express);
 
 describe('app', function(){
-  it('should expose app settings', function(){
+  it('should expose app settings', function(done){
     var obj = app.locals.settings;
     obj.should.have.property('env', 'test');
-    // expect(obj).to.have.property('appName', 'Test');
+    done();
   });
 });
 
 describe('sessions', function(){
   var store = new RedisStore;
+  var sessionData = {
+      cookie: {
+        maxAge: 2000
+      }
+    , name: 'tj' 
+  }
+  // TODO: Find out why this before statement times out
+  // Is there a risk that the store will not be instantiated when the tests run
   // before(function(done){
   //   store.client.on('connect', function(){
   //     done();
   //   });
-  //   // console.log(config.redis.port);
   // });
-  it('should be able to store sessions', function(){
-    // var store = new RedisStore;
-    // store.client.on('connect', function(){
-    // #set()
-    store.set('123', { cookie: { maxAge: 2000 }, name: 'tj' }, function(err, ok){
+  it('should be able to store sessions', function(done){
+    store.set('123', sessionData, function(err, ok){
       should.not.exist(err);
       should.exist(ok);
+      done();
     });
   });
-  it('should be able to retrieve sessions', function(){
+  it('should be able to retrieve sessions', function(done){
     store.get('123', function(err, data){
       should.not.exist(err);
       // console.log(data);
-      // data.should.equal({ cookie: { maxAge: 2000 }, name: 'tj' })
-      data.should.be.a('object').and.have.property('name', 'tj');
-      data.cookie.should.be.a('object').and.have.property('maxAge', 2000 );
+      data.should.be.a('object').and.have.property('name', sessionData.name);
+      data.cookie.should.be.a('object').and.have.property('maxAge', sessionData.cookie.maxAge );
+      done();
     });
   })
   after(function(done){
-    store.set('123', { cookie: { maxAge: 2000 }, name: 'tj' }, function(){
+    store.set('123', sessionData, function(){
       store.destroy('123', function(){
         store.client.end();
         done(); 
@@ -53,4 +58,4 @@ describe('in development', function(){
     app.enabled('view cache').should.be.false;
     process.env.NODE_ENV = 'test';
   })
-})
+});
---
+++ test/server.routes.js
@@ -0,0 +1,29 @@
+var app = require('../app')
+  , should = require('should')
+  , request = require('supertest')
+  , routes = require('../routes');
+
+describe('routes', function(){
+  it('should serve static files', function(done){
+    request(app)
+      .get('/coverage.html')
+      .expect(200, done);
+  })
+  it('should fail on bad routes', function(done){
+    request(app)
+      .get('/bad')
+      .expect(404, done);
+  });
+
+  // test the index route
+  describe('#index', function() {
+    it('should return index', function(done) {
+      request(app)
+        .get('/')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+});
